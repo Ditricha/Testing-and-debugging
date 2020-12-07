347b1914ae77c8ef488638d9fb8928db4701bdec
Ditricha <51129541+Ditricha@users.noreply.github.com>
2020-12-07 21:14:20 +0300

    Update MatrixMultSource.c

--- Project/MatrixMultSource.c
+++ Project/MatrixMultSource.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include "MatrixMult.h"
 
-//void print_matrix(int** matrix, int row, int col)
-//{
-//	int i, j;
-//	printf("\n");
-//	for (i = 0; i < row; i++) {
-//		for (j = 0; j < col; j++)
-//			printf("%d ", matrix[i][j]);
-//		printf("\n");
-//	}
-//}
 
 int** multiplication(int **matrix_1, int row1, int col1, int **matrix_2, int row2, int col2)
 {
@@ -26,7 +16,7 @@ int** multiplication(int **matrix_1, int row1, int col1, int **matrix_2, int row
 				resulting_matrix[i][j] += matrix_1[i][k] * matrix_2[k][j];
 		}
 
-	//âûâîä ðåçóëüòàòà óìíîæåíèÿ
+	//Ð²Ñ‹Ð²Ð¾Ð´ Ñ€ÐµÐ·ÑƒÐ»ÑŒÑ‚Ð°Ñ‚Ð° ÑƒÐ¼Ð½Ð¾Ð¶ÐµÐ½Ð¸Ñ
 	for (i = 0; i < row1; i++) {
 		for (j = 0; j < col2; j++)
 			printf("%d ", resulting_matrix[i][j]);
